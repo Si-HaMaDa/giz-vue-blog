aa795d5aafb8b2db89ec5fd62120b46d0b3b2cbd
Mohamed <nrookman_27@yahoo.com>
2022-09-29 14:57:38 +0200

    ContactPage BlogsPage with (SiderBar&BlogPost)

---
+++ src/components/blogs/BlogPost.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="col-lg-6">
+        <!-- Blog post-->
+        <div class="card mb-4">
+            <a href="single.html"
+                ><img
+                    class="card-img-top"
+                    src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                    alt="..."
+            /></a>
+            <div class="card-body">
+                <h2 class="card-title h4">{{ post.title }}</h2>
+                <p class="card-text">
+                    {{ post.desc }}
+                </p>
+                <a class="btn btn-primary" href="single.html">Read more →</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["post"],
+};
+</script>
